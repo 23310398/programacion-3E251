ee3169127049f464fc0a167c13a67137e2fc7412
23310398 <a23310398@live.ceti.mx>
2025-03-28 03:29:51 +0000

    practica 2

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,44 @@
 #include "Foco.hpp"
 #include "Serie.hpp"
 #include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+using namespace std;
+using namespace ftxui;
+
+
 int main(int argc, char const *argv[])
 {
-    Foco foco1;
-    Foco foco2;
+    auto Pantalla = Screen::Create(
+
+        Dimension::Full(),
+        Dimension::Full()
+    );
+
+    auto documento = vbox(
+        spinner(21,1
+        )
+    );
+
+    Render(Pantalla, documento);
+
+    Pantalla.Print();
+
+
+
+
+
+
+
+    // Foco foco1;
+    // Foco foco2;
     
-    foco1.Encender();
-    foco2.Apagar();
+    // foco1.Encender();
+    // foco2.Apagar();
 
-    foco1.Imprimir();
-    foco2.Imprimir();
-
-
+    // foco1.Imprimir();
+    // foco2.Imprimir();
 
 
     return 0;
-}
+};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # programacion-3E251
+
+ c++ Main.cpp -o programa -lftxui-component -lftxui-dom -lftxui-screen-std=c++17
+
+ ./programa
